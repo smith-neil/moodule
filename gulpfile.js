30b13d3cde785b1bf51a60cfb89004c19a720e3e
Neil Smith <nsmith@envoc.com>
2015-01-10 01:13:26 -0600

    chore: version gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,30 @@
 var gulp   = require('gulp'),
+    bump   = require('gulp-bump'),
     rename = require('gulp-rename'),
     uglify = require('gulp-uglify');
 
-gulp.task('default', function () {
+gulp.task('patch', ['build'], function () {
+  return gulp.src('./package.json')
+    .pipe(bump({type: 'patch'}))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('minor', ['build'], function () {
+  return gulp.src('./package.json')
+    .pipe(bump({type: 'minor'}))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('major', ['build'], function () {
+  return gulp.src('./package.json')
+    .pipe(bump({type: 'major'}))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('build', function () {
   return gulp.src(['./src/**/*.js', '!./src/**/*.spec.js'])
     .pipe(gulp.dest('./'))
     .pipe(uglify())
     .pipe(rename({extname: '.min.js'}))
     .pipe(gulp.dest('./'));
-})
+});
