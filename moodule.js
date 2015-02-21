1cafe021353046ed30c237a68cf71ae2f680da37
Neil Smith <nsmith@envoc.com>
2015-02-20 20:40:35 -0600

    build: 0.2.1

--- moodule.js
+++ moodule.js
@@ -22,7 +22,7 @@
 
     if (!isString(id))
       throw new Error("moodule says the module '" + id + "' is not a string.");
-    if (has(moduleDefinitions, id) || has(instances, id))
+    if (has(moduleDefinitions, id))
       throw new Error("moodule says the module '" + id + "' is already defined.");
     if (!isArray(dependencies))
       throw new Error("moodle says the dependencies must be an array.");
@@ -36,29 +36,29 @@
   }
 
   function require(id) {
-    try {
-      return resolve(id);
-    } catch (e) {
-      if (e instanceof RangeError)
-        throw new Error("moodule says circular dependencies are bad.");
-      else
-        throw e;
-    }
+    return resolve(id, []);
   }
 
-  function resolve(id) {
+  function resolve(id, dependencyCache) {
     if (has(instances, id)) {
       return instances[id];
     }
 
     var definition = moduleDefinitions[id];
+    dependencyCache.push(id);
     
     if (!definition)
       throw new Error("moodule says the module '" + id + "' is not defined.");
 
     var dependencies = [];
     for (var i = 0; i < definition.dependencies.length; i++) {
-      var dep = resolve(definition.dependencies[i]);
+      var dependency = definition.dependencies[i];
+
+      if (!has(instances, dependency))
+        if (has(dependencyCache, dependency))
+          throw new Error("moodule says circular dependencies are bad.");
+
+      var dep = resolve(definition.dependencies[i], dependencyCache);
       dependencies.push(dep);
     }
 
@@ -95,8 +95,18 @@
     return Object.prototype.toString.call(arg) === '[object ' + type + ']';
   }
 
-  function has(object, key) {
-    return object ? Object.prototype.hasOwnProperty.call(object, key) : false;
+  function has(objectOrArray, key) {
+    if (isObject(objectOrArray)) {
+      return objectOrArray ? Object.prototype.hasOwnProperty.call(objectOrArray, key) : false;
+    }
+    else if (isArray(objectOrArray)) {
+      for (var i = 0; i < objectOrArray.length; i++) {
+        if (objectOrArray[i] === key)
+          return true;
+      }
+    }     
+
+    return false;
   }
 
 });
